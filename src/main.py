1841b8f2493702e803fe74d1ad85b575cf378c17
Alexander Corr <dotnetcorr@gmail.com>
2019-03-17 22:45:12 +0500

    Added PySocks for socks5 proxy

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 import os
 import requests
+import socks
+
+# s = socks.socksocket()
+# s.set_proxy(socks.SOCKS5, "127.0.0.1", 9150)
+PROXY_LIST = {"https":"socks5://127.0.0.1:9150"}
 
 class Bot:
 
@@ -10,7 +15,7 @@ class Bot:
         params = {
             'timeout': 1,
         }
-        response = requests.post("https://api.telegram.org/bot%s/getMe" % self.token, params=params, timeout=10)
+        response = requests.post("https://api.telegram.org/bot%s/getMe" % self.token, params=params, timeout=10, proxies=PROXY_LIST)
         if response.ok:
             print(response)
         
